7148f3012746202d1c3307e337997d7691be2009
Hajin Jang <jb6804@naver.com>
2020-04-10 02:35:36 +0900

    Update prezto syntax highlighter styles

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 BASEDIR="${HOME}/.joveler"
 
+# Force install?
+ln_force_arg=""
+if [[ ${1} = "force" ]]; then
+    ln_force_arg="-f"
+fi
+
 declare -i count=0
 tfg_reset="\e[39m"
 tfg_yellow="\e[33m"
@@ -11,18 +17,32 @@ tfg_boldyellow="\e[93m"
 function install_config {
     conf_filename="${1##*/}"
 
-    if [[ -f "${HOME}/${conf_filename}" ]]; then
-        echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to overwrite existing ${tfg_yellow}${conf_filename}${tfg_reset}"
-        return 1
-    fi
-
-    ln -s "${BASEDIR}/$1" "${HOME}/${conf_filename}"
+    ln -s ${ln_force_arg} "${BASEDIR}/$1" "${HOME}/${conf_filename}"
     if [[ $? -eq 0 ]]; then
         echo -e "${tfg_boldgreen}[SUCCESS]${tfg_reset} Successfully installed ${tfg_yellow}${conf_filename}${tfg_reset}"
         count=$(( count + 1 ))
         return 0
     else
         echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to create symlink of ${tfg_yellow}${conf_filename}${tfg_reset}"
+        return 1
+    fi
+}
+
+function install_prezto_prompt {
+    conf_filename="${1##*/}"
+
+    if [[ -s "${HOME}/.zprezto" ]]; then
+        ln -s ${ln_force_arg} "${BASEDIR}/${1}" "${HOME}/.zprezto/modules/prompt/functions/prompt_${2}_setup"
+        if [[ $? -eq 0 ]]; then
+            echo -e "${tfg_boldgreen}[SUCCESS]${tfg_reset} Successfully installed ${tfg_yellow}custom prezto theme${tfg_reset}"
+            count=$(( count + 1 ))
+            return 0
+        else
+            echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to create symlink of ${tfg_yellow}${conf_filename}${tfg_reset}"
+            return 1
+        fi
+    else
+        echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to detect prezto. ${tfg_yellow}prezto theme${tfg_reset} was not installed."
         return 1
     fi
 }
@@ -37,32 +57,16 @@ install_config ".screenrc"
 install_config "vim/.vimrc"
 if [[ $? -eq 0 ]]; then
     vim +PluginInstall +qall
+    vim +PluginClean +qall
 fi
 
-# Install zsh config
-install_config ".zshrc"
-
-# Install zsh-prezto config
-install_config ".zlogin"
-install_config ".zpreztorc"
-install_config ".zprofile"
-install_config ".zshenv"
-
-# Install custom zprezto theme
-if [[ -s "${ZDOTDIR:-$HOME}/.zprezto" ]]; then
-    if [[ -f "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/prompt_joveler_setup" ]]; then
-        echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to overwrite existing ${tfg_yellow}prompt_joveler_setup${tfg_reset}"
-    else
-        ln -s "${BASEDIR}/zprezto-prompt/joveler.zsh" "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/prompt_joveler_setup"
-        if [[ $? -eq 0 ]]; then
-            echo -e "${tfg_boldgreen}[SUCCESS]${tfg_reset} Successfully installed ${tfg_yellow}custom prezto theme${tfg_reset}"
-        else
-            echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to create symlink of ${tfg_yellow}joveler.zsh${tfg_reset}"
-        fi
-    fi
-else
-    echo -e "${tfg_boldred}[WARNING]${tfg_reset} Unable to detect prezto. ${tfg_yellow}prezto theme${tfg_reset} was not installed."
-fi
+# Install zsh/prezto config
+install_config "zsh/.zshrc"
+install_config "zsh/.zlogin"
+install_config "zsh/.zpreztorc"
+install_config "zsh/.zprofile"
+install_config "zsh/.zshenv"
+install_prezto_prompt "zsh/prezto-prompt/joveler.zsh" "joveler"
 
 # Report result
 echo -e "Succesfully installed [${tfg_boldyellow}${count}${tfg_reset}] configs & files."
